0a6a83499e2e742a7903794416f22dbbf2d7bbcf
exso01 <amaksim666@gmail.com>
2025-01-26 19:26:05 +0300

    Delete about me directory

--- about me/script.js
+++
@@ -1,9 +0,0 @@
-// Открыть панель
-function openNav() {
-    document.getElementById("sideNav").style.width = "250px"; // Устанавливаем ширину панели
-}
-
-// Закрыть панель
-function closeNav() {
-    document.getElementById("sideNav").style.width = "0"; // Скрываем панель
-}
