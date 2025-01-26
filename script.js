ec204ce647006508c9ab6082f329c4a3c4ca1196
exso01 <amaksim666@gmail.com>
2025-01-26 19:30:17 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Открыть панель
+function openNav() {
+    document.getElementById("sideNav").style.width = "250px"; // Устанавливаем ширину панели
+}
+
+// Закрыть панель
+function closeNav() {
+    document.getElementById("sideNav").style.width = "0"; // Скрываем панель
+}
